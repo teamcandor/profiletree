cfbb1eba6955772930d9131219350db08c3afccd
Colin Bethea <colinjbethea@gmail.com>
2020-12-31 10:28:29 -0500

    Add endpoint steps

---
+++ pages/api/profile.js
@@ -0,0 +1,12 @@
+import axios from "axios"
+
+export default async (req, res) => {
+
+  // 1: Use Axios to GET data from 'https://pipl.ir/v1/getPerson'
+
+  // 2: Manipulate person's data to get only the data we want
+
+  // 3: Send both person + (predefined) posts to frontend
+
+  res.status(200).json({})
+}
