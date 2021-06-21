704a7d26864ba458a9e28bc90ef67716c0a66cf7
Colin Bethea <colinjbethea@gmail.com>
2021-06-21 10:47:35 -0700

    Add local storage comments

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,8 @@ const Index = () => {
   // 2: Our API endpoint is RESTful - we can get a user by querying /api/profile/id with the Fetch API
   // 3: Get the user with the following ID: 1OuR3CWOEsfISTpFxsG7  (eg: api/profile/${id})
   // 4: Additionally, pass a POST body parameter: isFullProfile: true
+  // 5: Can you implement local storage support (so we don't have to query profile each load)?
+  // 6: Even better, can you implement local storage as a custom hook?
 
   // * Hint * - Don't forget to pass headers with your request
 
